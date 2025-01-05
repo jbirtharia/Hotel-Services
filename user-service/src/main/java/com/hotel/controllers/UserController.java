e9ccd390fb046b6565ebc48abb4ec2649d2ced5e
jayendrabirtharia <jbirtharia@gmail.com>
2025-01-05 17:06:38 +0800

    Added Resilience Retry Pattern

--- user-service/src/main/java/com/hotel/controllers/UserController.java
+++ user-service/src/main/java/com/hotel/controllers/UserController.java
@@ -4,6 +4,8 @@ import com.hotel.entities.User;
 import com.hotel.fallbackhandler.RatingHotelFallbackHandler;
 import com.hotel.service.UserService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class UserController {
 
@@ -34,7 +37,9 @@ public class UserController {
     /// Circuit breaker for calling Ratings Service
     /// Need to pass method name in fallback which will send default response
     /// when the dependent services will be down.
-    @CircuitBreaker(name = "ratingsHotelsCircuitBreaker", fallbackMethod = "getRatingHotelsFallback")
+    //@CircuitBreaker(name = "ratingsHotelsCircuitBreaker", fallbackMethod = "getRatingHotelsFallback")
+    /// Implemented retry which will attempt to call below method 3 times if dependent service seems down.
+    @Retry(name = "retryForRatingsHotelsServices", fallbackMethod = "getRatingHotelsFallback")
     public ResponseEntity<User> getUser(@PathVariable String userId){
         return ResponseEntity.status(HttpStatus.OK).body(userService.getUserById(userId));
     }
